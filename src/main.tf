b409e9de58f2d6f0f67a8c67b6cc12b97c8ad4ba
jbowkett <james@bowkett.info>
2019-12-09 18:01:40 +0000

    added output of public ip

--- src/main.tf
+++ src/main.tf
@@ -2,11 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
+  ami = "ami-40d28157"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [
+    "${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World, startup date : `date` " > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+  EOF
   tags = {
-      Name = "terraform-example"
-    }
+    Name = "terraform-example"
+  }
 }
+variable "server_port" {
+  description = "The port used for http requests"
+  default = 8080
 
+}
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+output "public_ip"{
+  value = "${aws_instance.example.public_ip}"
+}
