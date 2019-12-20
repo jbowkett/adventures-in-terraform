7c97709e2b92a7ac67c953f449054d2ace280d4b
jbowkett <james@bowkett.info>
2019-12-20 08:57:15 +0000

    corrected the port on the aws security group - now accessible from the outside world!

--- src/main.tf
+++ src/main.tf
@@ -26,10 +26,88 @@ resource "aws_security_group" "instance" {
     from_port = var.server_port
     to_port = var.server_port
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 
 }
-output "public_ip"{
+output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
+output "debug"{
+  value = ["${data.aws_availability_zones.all.names}"]
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-40d28157"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World, startup date : `date` " > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+  EOF
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  availability_zones = data.aws_availability_zones.all.names
+  health_check_type = "ELB"
+  load_balancers = ["${aws_elb.example.name}"]
+  min_size = 2
+  max_size = 10
+  tag {
+    key = "name"
+    value = "terraform_asg_example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_elb" "example" {
+  name = "terraform-asg-example"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups = ["${aws_security_group.elb.id}"]
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = var.server_port
+    instance_protocol = "http"
+  }
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+}
+
