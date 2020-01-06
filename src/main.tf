1a010710fae32b1dff3c5648819955bc0cc6f69c
jbowkett <james@bowkett.info>
2020-01-06 17:33:01 +0000

    remove unused aws_instance

--- src/main.tf
+++ src/main.tf
@@ -1,20 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
-  ami = "ami-40d28157"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [
-    "${aws_security_group.instance.id}"]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World, startup date : `date` " > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-  EOF
-  tags = {
-    Name = "terraform-example"
-  }
-}
+
 variable "server_port" {
   description = "The port used for http requests"
   default = 8080
@@ -33,9 +20,6 @@ resource "aws_security_group" "instance" {
     create_before_destroy = true
   }
 
-}
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
 }
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
