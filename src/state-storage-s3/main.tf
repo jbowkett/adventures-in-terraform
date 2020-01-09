e2dade85ac37b51671154588743c0fccc8e8ec0f
jbowkett <james@bowkett.info>
2020-01-09 14:19:11 +0000

    added s3 as terraform state directory

---
+++ src/state-storage-s3/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-jb"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-jb"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
