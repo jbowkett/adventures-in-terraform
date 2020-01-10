4c1c0746c4536c356b87e4ed808d710e02760afa
jbowkett <james@bowkett.info>
2020-01-10 08:34:21 +0000

    add a mysql database resource

---
+++ src/stage/data-stores/mysql/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-jb"
+    key    = "state/services/mysql-cluster/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = "true"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+  engine            = "mysql"
+  identifier        = "jb-example-database-2"
+  allocated_storage = 10
+  instance_class    = "db.t2.micro"
+  name              = "example_database_2"
+  username          = "admin"
+  password          = "${var.db_password}"
+  skip_final_snapshot = true
+}
